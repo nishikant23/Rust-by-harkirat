94cf5badf3a8085c96298f4f93d5bf95f013cd9c
Nishikant R Dounekar <119045534+nishikant23@users.noreply.github.com>
2024-05-16 20:58:39 +0530

    Charaters in RUST and its Options

--- src/main.rs
+++ src/main.rs
@@ -30,5 +30,19 @@ fn main() {
     // unlike ints, we cant able to reserved size for string, 
     let greetings = String::from("Hello, everyone I am learning rust");
     println!("{}",greetings);  //way of printing any data in RUST curlyBraces{}
+
+    //Character Dealing
+    //JS greeting[2] -> [print 2nd index of str char]
+    //RUST greeting[2] or greeting.chars().nth(2) -> find at nth =2th index char +> throw error
+    //bcos, RUST is Compile time lang, it says I dont know if str's nth char u gave isCharPresnt or nothing there(str ended)
+    // we as devloper cautious while coding there we use: match char{}+avoid error OR char.unwrap() we OK with compile error
+
+    let char1 = greetings.chars().nth(3);
+
+    match char1 {   //Error Catching syntax 
+        Some(c)=> {println!("{}", c)},
+        None => println!("No charater at nth index"),
+    }
     
+    //println!("{}", char1.unwrap());   Another way to avoid  but throw error   
 }
