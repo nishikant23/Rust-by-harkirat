aaf3f9cfad142b1dc40452636cfd138b476cc0cb
Nishikant R Dounekar <119045534+nishikant23@users.noreply.github.com>
2024-05-16 19:21:40 +0530

    Rust boolean vars

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,24 @@ fn main() {
 
     println!("x:{}, y:{}, z:{}",x,y,z);
 
-    let mut a = 10;
+    let mut a = 10; //mut = mutable can change same var values 
     for i in 0..100{
         a = a+i;
     }
     print!("a:{}", a);
      
+    let mut is_male: bool = false;
+    let is_above_18: bool = true;
 
+    if is_male {
+        println!("Your are male ");
+    } else {
+        println!("Your are not male");
+    }
+     
+    is_male = true; //re-defined & value changed since is_male is mutable
+    if is_male && is_above_18 {
+        print!("you are legal to drive & Legal Male");
+    } 
     
 }
